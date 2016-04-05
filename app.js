5eb342a07c85c289671f9bce916dc09180fafec8
zhaoweilong <zhaoweilong@breadtrip.com>
2016-04-05 10:04:29 +0800

    finish code

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('./path/private.pem', 'utf8');
+var certificate = fs.readFileSync('./path/file.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+var PORT = 6000;
+var SSLPORT = 6001;
+
+httpServer.listen(PORT, function() {
+    console.log('HTTP Server is running on: http://localhost:%s', PORT);
+});
+httpsServer.listen(SSLPORT, function() {
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+});
+
+// Welcome
+app.get('/', function(req, res) {
+    if(req.protocol === 'https') {
+        res.status(200).send('Welcome to Safety Land!');
+    }
+    else {
+        res.status(200).send('Welcome!');
+    }
+});
